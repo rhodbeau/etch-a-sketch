6fa7c0332aa850831971bd0be8a076bb95e92d3a
Rhonda <rhodbeau@yahoo.com>
2022-08-17 19:01:43 -0500

    Create index.html
    Create style.css and javascript.js
    Create 16x16 grid of divs

---
+++ javascript.js
@@ -0,0 +1,13 @@
+const container = document.querySelector(".container");
+
+for (let i = 0; i < 16; i++) {
+    let row = document.createElement("div");
+    row.classList.add("row");
+    for (let j = 0; j < 16; j++) {
+        let div = document.createElement("div");
+        div.classList.add('item');
+        div.setAttribute('style', 'border-color: black; border-width: thin; border-style: solid;');
+        row.appendChild(div);
+    }
+    container.appendChild(row);
+}
